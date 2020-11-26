2ed92fff68ce58f9c6d9f44dadfdd0a9a29c278c
jhahspu <jhahspu@gmail.com>
2020-11-26 14:35:28 +0000

    file samples

--- createProject.py
+++ createProject.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import pathlib
 
 def save_to_file(content, filename):
     with open(filename, 'w') as file:
@@ -10,10 +11,11 @@ def createLocalPrj(fn):
     print('creating project: ' + fn + ' at: ' + fdn)
     os.mkdir(fdn)
     os.chdir(fdn)
-    save_to_file("## Hello World \n### Some text on a new line line...", "readme.md")
+    save_to_file("## Hello World \n### Another line...", "readme.md")
 
 if (len(sys.argv) > 3):
     print('creating local project: ' + str(sys.argv[1]) + ' ' + str(sys.argv[2]) + ' ' + str(sys.argv[3]))
     createLocalPrj(str(sys.argv[1]))
 else:
     print('creating remote project: ' + str(sys.argv[1]) + ' ' + str(sys.argv[2]))
+
